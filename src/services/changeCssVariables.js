713da8610df777d8283913a6d848d1369a23ec43
Vladimir F <vof9696@gmail.com>
2023-04-07 18:52:48 +0300

    fix changeCssVariables

--- src/services/changeCssVariables.js
+++ src/services/changeCssVariables.js
@@ -1,6 +1,9 @@
-export const changeCssVariables = theme => {
-	const root = document.querySelector(':root')
-	console.log(root.style,theme)
-	root.style.setProperty('--background-default-img',`var(--background-${theme}-img)`); 
-	// root.style.setProperty(`$body-color`,`$body-color${theme}`,'important') 
-}
+export const changeCssVariables = (theme) => {
+	const root = document.querySelector(':root');
+	console.log(root.style, theme);
+
+	const CssVariables = ['header', 'bgimg'];
+	CssVariables.forEach(element => {
+		root.style.setProperty(`--theme-default-${element}`, `var(--theme-${theme}-${element})`);
+	});
+};
