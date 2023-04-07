a5b05ac9014aa6d36f1fcc5f0636d3145b93903a
Vladimir F <vof9696@gmail.com>
2023-04-07 19:56:25 +0300

    fix

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
 	return (
 		<>
-			<BrowserRouter browsername={`//${REPO_NAME}`}>
+			<BrowserRouter basename={`/${REPO_NAME}/`}>
 				<Navbar />
 				<Routes>
 					{routerConfig.map((route, index) => (
